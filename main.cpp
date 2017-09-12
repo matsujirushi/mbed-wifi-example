16bef5dbae7bb5b4246f65da72d708b284d25840
zhangjian <zhangjian@mxchip.com>
2017-09-12 15:48:38 +0800

    Add ble proximity demo

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 
 #define RUN_APPLICATION( app )     extern int app_##app(void); return app_##app();
 
+extern "C" int app_ble_proximity_reporter(void);
+
 int main( void )
 {
-   /* APPLICATION can be assigned to the folder names under folder "APP" */
-   //RUN_APPLICATION( iperf );
+    /* APPLICATION can be assigned to the folder names under folder "APP" */
+//    RUN_APPLICATION( iperf );
 //    RUN_APPLICATION( sirc_transmit );
-   //RUN_APPLICATION( mbed_wifi );
-   //RUN_APPLICATION( mbed_tls_client );
-   //RUN_APPLICATION( mbed_tcp_udp );
+//    RUN_APPLICATION( mbed_wifi );
+//    RUN_APPLICATION( mbed_tls_client );
+//    RUN_APPLICATION( mbed_tcp_udp );
 //    RUN_APPLICATION( soft_ap );
-   RUN_APPLICATION( audio );
+//    RUN_APPLICATION( audio );
 
+//    RUN_APPLICATION(bt_rfcomm_server);
+    return app_ble_proximity_reporter();
 	return 0;
 }
