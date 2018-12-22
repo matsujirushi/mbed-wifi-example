1853ed1527c045bda977983b26d6f747bebb4787
Takashi Matsuoka <matsujirushi@live.jp>
2018-12-22 18:03:24 +0900

    Update mbed-emw10xx-driver.lib

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main( void )
    /* APPLICATION can be assigned to the folder names under folder "APP" */
    //RUN_APPLICATION( iperf );
 //    RUN_APPLICATION( sirc_transmit );
-   //RUN_APPLICATION( mbed_wifi );
+   RUN_APPLICATION( mbed_wifi );
    //RUN_APPLICATION( mbed_tls_client );
    //RUN_APPLICATION( mbed_tcp_udp );
 //    RUN_APPLICATION( soft_ap );
-   RUN_APPLICATION( audio );
+   //RUN_APPLICATION( audio );
 
 	return 0;
 }
